4ef6d695f3e6c3d3f371f8334c612e926f0d9888
Stepan Kurennykh <stoope@outlook.com>
2026-02-27 17:22:28 +0300

    Add conditional Adjust SDK imports and usage safeguards

--- Sources/KovaleeAttribution/AdjustWrapperImpl.swift
+++ Sources/KovaleeAttribution/AdjustWrapperImpl.swift
@@ -1,5 +1,6 @@
 #if canImport(AdjustSdk)
-import AdjustSdk
+    import AdjustSdk
+#endif
 import AdSupport
 import AppTrackingTransparency
 import Foundation
@@ -34,38 +35,50 @@ final class AdjustWrapperImpl: NSObject, AttributionManager, Manager {
         self.configuration = configuration
         super.init()
 
-        let adjustConfig = ADJConfig(
-            appToken: configuration.token,
-            environment: configuration.environment.adjustEnvironment
-        )
-        adjustConfig?.logLevel = KLogger.logLevel.adjustLogLevel()
-        adjustConfig?.attConsentWaitingInterval = 3
-        adjustConfig?.delegate = self
+    #if canImport(AdjustSdk)
+            let adjustConfig = ADJConfig(
+                appToken: configuration.token,
+                environment: configuration.environment.adjustEnvironment
+            )
+            adjustConfig?.logLevel = KLogger.logLevel.adjustLogLevel()
+            adjustConfig?.attConsentWaitingInterval = 3
+            adjustConfig?.delegate = self
 
-        Adjust.initSdk(adjustConfig)
+            Adjust.initSdk(adjustConfig)
+    #endif
     }
 
     func setDataCollectionEnabled(_ enabled: Bool) {
-        guard let value = ADJThirdPartySharing(isEnabled: enabled ? 1 : 0) else {
-            return
-        }
-        Adjust.trackThirdPartySharing(value)
+    #if canImport(AdjustSdk)
+            guard let value = ADJThirdPartySharing(isEnabled: enabled ? 1 : 0) else {
+                return
+            }
+            Adjust.trackThirdPartySharing(value)
+    #endif
     }
 
     func getAttributionAdid() async -> String? {
-        await Adjust.adid()
+    #if canImport(AdjustSdk)
+            await Adjust.adid()
+    #endif
     }
 
     func promptTrackingAuthorization(completion: @escaping (ATTrackingManager.AuthorizationStatus) -> Void) {
-        Adjust.requestAppTrackingAuthorization { _ in
-            completion(ATTrackingManager.trackingAuthorizationStatus)
-        }
+    #if canImport(AdjustSdk)
+            Adjust.requestAppTrackingAuthorization { _ in
+                completion(ATTrackingManager.trackingAuthorizationStatus)
+            }
+    #else
+            ATTrackingManager.requestTrackingAuthorization(completionHandler: completion)
+    #endif
     }
 
     func sendConversionValue(value: Int, coarseValue: String?, completion: @escaping (Error?) -> Void) {
-        Adjust.updateSkanConversionValue(value, coarseValue: coarseValue, lockWindow: false) { error in
-            completion(error)
-        }
+        #if canImport(AdjustSdk)
+            Adjust.updateSkanConversionValue(value, coarseValue: coarseValue, lockWindow: false) { error in
+                completion(error)
+            }
+        #endif
     }
 
     func setAttributionDelegate(_ delegate: KovaleeFramework.KovaleeAttributionDelegate) {
@@ -80,19 +93,21 @@ final class AdjustWrapperImpl: NSObject, AttributionManager, Manager {
 
 extension AdjustWrapperImpl: @unchecked Sendable {}
 
-extension AdjustWrapperImpl: AdjustDelegate {
-    func adjustSessionTrackingSucceeded(_ session: ADJSessionSuccess?) {
-        attributionAdidCallback(session?.adid)
-    }
+#if canImport(AdjustSdk)
+    extension AdjustWrapperImpl: AdjustDelegate {
+        func adjustSessionTrackingSucceeded(_ session: ADJSessionSuccess?) {
+            attributionAdidCallback(session?.adid)
+        }
 
-    func adjustConversionValueUpdated(_ fineValue: NSNumber?, coarseValue _: String?, lockWindow _: NSNumber?) {
-        KLogger.debug("Successfully Updated Conversion Value: \(String(describing: fineValue))")
-    }
+        func adjustConversionValueUpdated(_ fineValue: NSNumber?, coarseValue _: String?, lockWindow _: NSNumber?) {
+            KLogger.debug("Successfully Updated Conversion Value: \(String(describing: fineValue))")
+        }
 
-    func adjustDeferredDeeplinkReceived(_ deeplink: URL?) -> Bool {
-        delegate?.adjustDeferredDeeplinkReceived(deeplink) ?? true
+        func adjustDeferredDeeplinkReceived(_ deeplink: URL?) -> Bool {
+            delegate?.adjustDeferredDeeplinkReceived(deeplink) ?? true
+        }
     }
-}
+#endif
 
 extension AttributionManager {
     static var test: AttributionManager {
@@ -103,22 +118,23 @@ extension AttributionManager {
     }
 }
 
-extension KovaleeFramework.LogLevel {
-    func adjustLogLevel() -> ADJLogLevel {
-        switch self {
-        case .verbose:
-            .verbose
-        case .debug:
-            .debug
-        case .info:
-            .info
-        case .warn:
-            .warn
-        case .error:
-            .error
-        @unknown default:
-            fatalError()
+#if canImport(AdjustSdk)
+    extension KovaleeFramework.LogLevel {
+        func adjustLogLevel() -> ADJLogLevel {
+            switch self {
+            case .verbose:
+                .verbose
+            case .debug:
+                .debug
+            case .info:
+                .info
+            case .warn:
+                .warn
+            case .error:
+                .error
+            @unknown default:
+                fatalError()
+            }
         }
     }
-}
 #endif
